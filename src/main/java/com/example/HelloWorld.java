af64d7b0a2acbe9afafb6b024d1fd0d8323bf0c7
kirande1 <Kirandeep@amdocs.com>
2025-08-10 18:42:37 -0400

    Modified

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,7 +2,7 @@ package main.java.com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Jenkins CI/CD!");
     }
 
     public String greet(String name) {
